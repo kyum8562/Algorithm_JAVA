05928667d6fa953abf0b8139ac02f8833fdf91ae
김창겸 <kyum8562@naver.com>
2023-06-14 03:16:15 +0900

    0614

---
+++ src/com/company/_2098_외판원순회.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+
+public class _2098_외판원순회 {
+    static int N;
+    static int[][] map;
+    static boolean[][] v;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+    }
+}
