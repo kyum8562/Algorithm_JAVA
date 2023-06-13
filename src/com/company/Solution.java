a445685bf821df41a97c2093aad11febbac9eb3f
김창겸 <kyum8562@naver.com>
2023-06-14 03:16:37 +0900

    0614

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -7,15 +7,14 @@ public class Solution {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            if (a == 0 && b == 0) return;
-            System.out.println(a + b);
-
-        }
-
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+            System.out.println(a%b);
 
     }
 }
