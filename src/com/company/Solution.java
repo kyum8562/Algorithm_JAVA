7122807e388cc14bdacc2c027f329f3d78488179
김창겸 <kyum8562@naver.com>
2023-06-16 12:29:16 +0900

    0616

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -8,16 +8,13 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            String[] arr = br.readLine().split(" ");
-            int ans1 = Integer.MAX_VALUE;
-            int ans2 = Integer.MIN_VALUE;
+        int N = Integer.parseInt(st.nextToken());
+        String s = br.readLine();
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            ans += s.charAt(i) - '0';
+        }
 
-            for(int i = 0 ; i < N ; i ++){
-                ans1 = Math.min(ans1 , Integer.parseInt(arr[i]));
-                ans2 = Math.max(ans2 , Integer.parseInt(arr[i]));
-            }
-        System.out.println(ans1+" "+ ans2);
-
+        System.out.println(ans);
     }
 }
