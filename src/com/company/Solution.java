5f9bd95842ddb04ac03b0bd6afb5b9d45046a8af
김창겸 <kyum8562@naver.com>
2023-06-17 08:34:38 +0900

    0617

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -9,12 +9,6 @@ public class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        String s = br.readLine();
-        int ans = 0;
-        for (int i = 0; i < N; i++) {
-            ans += s.charAt(i) - '0';
-        }
 
-        System.out.println(ans);
     }
 }
