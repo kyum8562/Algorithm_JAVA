08bc5479a8fe5462916a5c6161335860d91498dc
김창겸 <kyum8562@naver.com>
2023-06-18 23:53:03 +0900

    0618

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -7,8 +7,6 @@ public class Solution {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-
+        System.out.println(br.readLine() + "??!");
     }
 }
