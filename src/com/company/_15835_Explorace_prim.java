b05c506f00e4d93eba43eb1efb0dd7a98a36408e
김창겸 <kyum8562@naver.com>
2023-06-25 20:49:22 +0900

    0625

---
+++ src/com/company/_15835_Explorace_prim.java
@@ -0,0 +1,76 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class _15835_Explorace_prim {
+    static int N, M;
+    static int ans;
+    static List<Node>[] graph;
+    static boolean[] v;
+
+    static class Node {
+        int node;
+        int dist;
+
+        public Node(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int t = 0;
+        while (t++ < T) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+            ans = 0;
+            v = new boolean[N + 1];
+            graph = new ArrayList[N + 1];
+            for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
+
+            for (int i = 1; i <= M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                int d = Integer.parseInt(st.nextToken());
+
+                graph[s].add(new Node(e, d));
+                graph[e].add(new Node(s, d));
+            }
+
+
+            prim(1);
+            System.out.println("Case #" +t+ ": " + ans + " meters");
+
+        }
+    }
+
+    private static void prim(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.dist - o2.dist);
+        pq.offer(new Node(start, 0));
+
+        while(!pq.isEmpty()){
+            Node currNode = pq.poll();
+            int node = currNode.node;
+            int dist = currNode.dist;
+
+            if(v[node]) continue;
+            v[node] = true;
+            ans += dist;
+
+            for(Node next: graph[node]){
+                if(!v[next.node]) pq.offer(next);
+            }
+        }
+    }
+}
