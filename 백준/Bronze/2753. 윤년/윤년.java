bf5a7f1163070823e6fbedcb3c126851d50d5bee
giraffe_kim <kyum8562@gmail.com>
2023-07-05 23:25:07 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int ans = (A%4 == 0 && (A%100 != 0 || A%400 == 0)) ? 1 : 0;
+        System.out.println(ans);
+    }
+}
