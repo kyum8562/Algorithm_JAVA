c77c8be875c8c70b7f836aa543d1ca6171f8c1a8
giraffe_kim <kyum8562@gmail.com>
2023-07-07 17:25:19 +0900

    [Bronze III] Title: 공 넣기, Time: 136 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N+1];
+
+
+        for(int i = 1 ; i <= M ; i ++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            for(int j = a ; j <= b ; j ++){
+                arr[j] = c;
+            }
+        }
+
+        for(int i = 1 ; i <= N ; i ++) System.out.print(arr[i] + " ");
+
+
+    }
+}
