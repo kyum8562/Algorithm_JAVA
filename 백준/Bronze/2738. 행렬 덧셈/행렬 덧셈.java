1f35d1775ef31161d796682c7bdf7a4148a92932
giraffe_kim <kyum8562@gmail.com>
2023-07-10 02:06:39 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 364 ms, Memory: 18592 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[N][M];
+        for(int i = 0 ; i < N ; i ++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0 ; j < M ; j ++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0 ; i < N ; i ++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0 ; j < M ; j ++){
+                map[i][j] += Integer.parseInt(st.nextToken());
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
