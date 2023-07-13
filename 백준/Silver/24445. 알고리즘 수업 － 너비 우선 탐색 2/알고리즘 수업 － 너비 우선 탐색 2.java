39f1d8f01511c89230814d7fe76d638b4088f6aa
giraffe_kim <kyum8562@gmail.com>
2023-07-13 12:19:33 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1840 ms, Memory: 102296 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, K, cnt = 1;
+    static int[] v;
+    static List<Integer>[] list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        v = new int[N + 1];
+        list = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) list[i] = new ArrayList<>();
+
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list[a].add(b);
+            list[b].add(a);
+        }
+
+        for (int i = 1; i <= N; i++) Collections.sort(list[i], Collections.reverseOrder());
+
+        bfs(K);
+
+        for (int i = 1; i <= N; i++) System.out.println(v[i]);
+
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(start);
+
+        while (!q.isEmpty()) {
+            int qSize = q.size();
+            int curr = q.poll();
+
+            if (v[curr] != 0) continue;
+            v[curr] = cnt++;
+
+            for (int next : list[curr]) {
+                if (v[next] == 0) q.offer(next);
+            }
+
+        }
+    }
+}
