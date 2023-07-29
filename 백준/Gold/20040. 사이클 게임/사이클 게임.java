17bb1d7435c0fcfe0a6f08bf0661cb013a3b70f9
giraffe_kim <kyum8562@gmail.com>
2023-07-30 01:35:43 +0900

    [Gold IV] Title: 사이클 게임, Time: 656 ms, Memory: 119804 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] parent;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        parent = new int[N+1];
+
+        for(int i = 1 ; i <= N ; i ++) parent[i] = i;
+
+
+        for(int i = 1 ; i <= M ; i ++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) + 1;
+            int b = Integer.parseInt(st.nextToken()) + 1;
+
+            // 부모가 같은지 검사하고 같다면 i 리턴
+            if(sameParent(a, b)){
+                System.out.println(i);
+                return;
+            }
+
+            union(a, b);
+        }
+
+        System.out.println(0);
+    }
+
+    // 부모 찾기
+    private static int find(int x) {
+        if(x == parent[x]) return x;
+        else return parent[x] = find(parent[x]);
+    }
+    // 합집합
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x != y){
+            if(x < y) parent[y] = x;
+            else parent[x] = y;
+        }
+    }
+    // 부모가 같은지 검사
+    private static boolean sameParent(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x == y) return true;
+        else return false;
+    }
+
+}
