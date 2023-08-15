5ce26ecc3dfb874852b6736b892ef8d62432b483
giraffe_kim <kyum8562@gmail.com>
2023-08-15 17:41:07 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1440 ms, Memory: 125612 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -21,37 +21,33 @@ public class Main {
 
         M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++){
+        for (int i = 0; i < M; i++) {
             int tgt = Integer.parseInt(st.nextToken());
-            sb.append(upperBound(tgt) - lowerBound(tgt)).append(" ");
+//            sb.append(upperBound(tgt) - lowerBound(tgt)).append(" ");
+            sb.append(bound(tgt, 1) - bound(tgt, 0)).append(" ");
         }
 
         System.out.println(sb);
     }
 
-    private static int lowerBound(int tgt) {
+    private static int bound(int tgt, int type) {
         int start = 0;
         int mid = 0;
         int end = N;
 
         while (end > start) {
-            mid = (end + start) / 2;
+            mid = (start + end) / 2;
 
-            if (arr[mid] >= tgt) end = mid;
-            else start = mid + 1;
-        }
-        return start;
-    }
-    private static int upperBound(int tgt) {
-        int start = 0;
-        int mid = 0;
-        int end = N;
-
-        while (end > start) {
-            mid = (end + start) / 2;
-
-            if (arr[mid] > tgt) end = mid;
-            else start = mid + 1;
+            // lower
+            if(type == 0){
+                if(arr[mid] >= tgt) end = mid;
+                else start = mid + 1;
+            }
+            // upper
+            else {
+                if(arr[mid] > tgt) end = mid;
+                else start = mid + 1;
+            }
         }
         return start;
     }
