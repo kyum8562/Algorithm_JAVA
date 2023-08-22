b75194384b65c8187e6fe7d2b63ca481b2639664
giraffe_kim <kyum8562@gmail.com>
2023-08-22 15:35:40 +0900

    [Gold III] Title: 양 구출 작전, Time: 1312 ms, Memory: 73340 KB -BaekjoonHub

--- 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
+++ 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
@@ -1,27 +1,15 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
     static int N;
-    static List<Node>[] list;
-
-    static class Node {
-        char type;
-        int node;
-        int dist;
-
-        public Node(char type, int node, int dist) {
-            this.type = type;
-            this.node = node;
-            this.dist = dist;
-        }
-    }
-
+    static StringTokenizer st;
+    static List<Integer>[] list;
+    static long[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         N = Integer.parseInt(br.readLine());
+
+        dp = new long[N + 1];
         list = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) list[i] = new ArrayList<>();
 
@@ -31,18 +19,21 @@ public class Main {
             int b = Integer.parseInt(st.nextToken()); // a의 개체 수
             int c = Integer.parseInt(st.nextToken()); // 해당 i섬에는 c로 가는 다리가 있음
 
-            list[c].add(new Node(a, i, b)); // 1에서 아래로 내려갈 것
+            list[c].add(i);
+            dp[i] = (a == 'W') ? b*-1 : b;
         }
 
-        System.out.println(dfs(new Node('X', 1, 0)));
+        dfs(1, -1);
+        System.out.println(dp[1]);
     }
 
-    private static long dfs(Node curr) {
-        long sum = 0;
-        for (Node next : list[curr.node]) {
-            sum += dfs(next);
+    private static void dfs(int curr, int parent) {
+        for (int next : list[curr]) {
+            dfs(next, curr);
         }
-        if (curr.type == 'S') return sum + curr.dist;
-        else return (sum - curr.dist > 0) ? sum - curr.dist : 0;
+        if(parent != -1){
+            if(dp[curr] > 0)
+                dp[parent] += dp[curr];
+        }
     }
 }
