874453ca53d8ddf02b0aa87e75f50d8ee67d4176
giraffe_kim <kyum8562@gmail.com>
2023-08-30 21:39:28 +0900

    [Gold IV] Title: 치즈, Time: 156 ms, Memory: 15276 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -2,12 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
+    static int N, M, melt, cheese = 0;
     static int[][] map;
     static boolean[][] v;
     static int[] dr = {-1, 0, 1, 0};
     static int[] dc = {0, 1, 0, -1};
-    static List<Integer> saveCheeseCnt = new ArrayList<>();
     static class Node {
         int r;
         int c;
@@ -17,7 +16,6 @@ public class Main {
             this.c = c;
         }
     }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -29,17 +27,19 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++)
+            for (int j = 0; j < M; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 1) cheese ++;
+            }
         }
 
-        boolean flag = true;
         int time = 0;
-        int firstCheeseCnt = getCheeseCnt();
 
-        while (flag) {
+        while (cheese != 0) {
 
             time++;
+
+            melt = 0;
 
             // bfs 탐색: 가장자리 치즈 탐색
             bfs(0, 0);
@@ -50,19 +50,14 @@ public class Main {
             // 방문 초기화
             v = new boolean[N][M];
 
-            // 치즈 개수 파악(마지막 치즈 개수 확인을 위해)
-            int currCheeseCnt = getCheeseCnt();
-
-            if(currCheeseCnt == 0) flag = false;
-            else saveCheeseCnt.add(currCheeseCnt);
+            // cheese 업데이트
+            cheese -= melt;
         }
 
         System.out.println(time);
-        if(saveCheeseCnt.size() > 0) System.out.println(saveCheeseCnt.get(saveCheeseCnt.size() - 1));
-        else System.out.println(firstCheeseCnt);
+        System.out.println(melt);
 
     }
-
     private static void bfs(int i, int j) {
         Queue<Node> q = new ArrayDeque<>();
         v[i][j] = true;
@@ -87,20 +82,13 @@ public class Main {
             }
         }
     }
-    private static int getCheeseCnt() {
-        int cheeseCnt = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 1) cheeseCnt++;
-            }
-        }
-        return cheeseCnt;
-    }
     private static void removeCheese() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if(map[i][j] == 2)
+                if(map[i][j] == 2){
                     map[i][j] = 0;
+                    melt ++;
+                }
             }
         }
     }
