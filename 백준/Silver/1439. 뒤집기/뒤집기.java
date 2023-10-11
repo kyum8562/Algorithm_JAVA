a406d8c5fe0f2956c007cc8e9ae7cabf8891892a
giraffe_kim <kyum8562@gmail.com>
2023-10-11 15:14:05 +0900

    [Silver V] Title: 뒤집기, Time: 124 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -3,47 +3,38 @@ import java.util.*;
 
 public class Main {
     static int sLen;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
+
         sLen = s.length();
-        int[] arr = new int[sLen];
-        for (int i = 0; i < sLen; i++) {
+        arr = new int[sLen];
+        for (int i = 0; i < sLen; i++)
             arr[i] = s.charAt(i) - '0';
-        }
 
-        int cnt = 0;
-
-        int idx = 0;
-        while (idx < sLen) {
-            if (arr[idx] == 0) {
-                cnt ++;
-                int tmp = 0;
-                for (int i = idx; i < sLen; i++) {
-                    if (arr[i] == 0) tmp ++;
-                    else break;
-                }
-                idx += tmp;
-            }
-            else idx++;
-        }
-
-        int cnt2 = 0;
-        idx = 0;
-        while (idx < sLen) {
-            if (arr[idx] == 1) {
-                cnt2 ++;
-                int tmp = 0;
-                for (int i = idx; i < sLen; i++) {
-                    if (arr[i] == 1) tmp ++;
-                    else break;
-                }
-                idx += tmp;
-            }
-            else idx++;
-        }
+        int cnt = play(0, 0);
+        int cnt2 = play(0, 1);
 
         System.out.println(Math.min(cnt, cnt2));
+    }
+
+    private static int play(int idx, int type) {
+        int res = 0;
+
+        while (idx < sLen) {
+            if (arr[idx] == type) {
+                res ++;
+                int tmp = 0;
+                for (int i = idx; i < sLen; i++) {
+                    if (arr[i] == type) tmp ++;
+                    else break;
+                }
+                idx += tmp;
+            }
+            else idx++;
+        }
+        return res;
     }
 }
