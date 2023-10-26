086183ce3771f7d5d0135d923036646095380931
giraffe_kim <kyum8562@gmail.com>
2023-10-26 16:06:05 +0900

    [Silver II] Title: 베르트랑 공준, Time: 140 ms, Memory: 15420 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
  
 public class Main {
 	/*
@@ -6,24 +8,29 @@ public class Main {
 	0 부터 시작하므로 246912 + 1
 	*/
 	public static boolean[] prime = new boolean[246913];
-    
-	public static void main(String[] args) {
  
-		Scanner in = new Scanner(System.in);
+	/*
+	1 부터 누적하여 각 index 까지의 
+	소수의 개수를 담을 배열
+	*/
+	public static int[] count_arr = new int[246913];
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
 		get_prime();	// 소수를 얻는 메소드 실행
-		
+		get_count();	// 소수의 개수가 저장된 배열을 얻는 메소드 실행
+		StringBuilder sb = new StringBuilder();
+ 
 		while(true) {
-			int n = in.nextInt();
+			int n = Integer.parseInt(br.readLine());
 			if(n == 0) break;	// n 이 0 일경우 종료
-            
-			int count = 0;	// 소수 개수를 셀 변수
-            
-			for(int i = n + 1; i <= 2 * n; i++) {
-				if(!prime[i]) count++;
-			}
-			System.out.println(count);
-		}		
+ 
+			// 2n 까지의 소수의 개수 - n 까지의 소수의 개수
+			sb.append(count_arr[2 * n] - count_arr[n]).append('\n');
+		}
+		System.out.print(sb);
 	}
 	
 	// 소수를 얻는 메소드
@@ -38,5 +45,20 @@ public class Main {
 			}
 		}
 	}
+    
+	// 소수의 개수를 얻는 메소드
+	public static void get_count() {
+		int count = 0;
+		for(int i = 2; i < prime.length; i++) {
+			if(!prime[i]) count++;	// 소수일 경우 count를 증가시킨다
+            
+			/*
+			0 ~ i 까지 소수의 개수 = count
+			count 값을 count_arr 의 i 에 저장한다
+			*/
+			count_arr[i] = count;	
+		}
+	}
+ 
  
 }
