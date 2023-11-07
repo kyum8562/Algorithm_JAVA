0bab3bec10a3f9e5166cb11e331e0ee2f9bea8cd
giraffe_kim <kyum8562@gmail.com>
2023-11-07 19:19:39 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,14 +1,41 @@
 function solution(ingredient) {
-  let answer = 0;
-  const stack = [];
-
-  for (let i of ingredient) {
-    stack.push(i);
-
-    if (stack.slice(-4).join('') == '1231') {
-      stack.splice(-4);
-      answer++;
+    let stk = [];
+    let count = 0;
+    for (let i = 0; i < ingredient.length; i++) {
+        stk.push(ingredient[i]);
+        if (
+            stk[stk.length-1] === 1 &&
+            stk[stk.length-2] === 3 &&
+            stk[stk.length-3] === 2 &&
+            stk[stk.length-4] === 1
+        ) {
+            count++;
+            stk.splice(-4);
+        }
     }
-  }
-  return answer;
+    return count;
 }
+
+/**
+function solution(ingredient) {
+    const stk = [];
+    let cnt = 0;
+    
+    ingredient.forEach((ing, idx)=>{
+        stk.push(ing);  
+       
+        if(stk.length >= 4){
+            const find = stk.slice(-4).join('');
+            if(find === '1231'){
+                stk.pop();
+                stk.pop();
+                stk.pop();
+                stk.pop();   
+                cnt++;
+            }
+        } 
+    })
+    
+    return cnt;
+}
+**/
