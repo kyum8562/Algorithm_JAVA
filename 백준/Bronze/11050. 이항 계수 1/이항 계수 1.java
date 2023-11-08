e8736609b27def7107dc688b69cda14a63d4ebbe
giraffe_kim <kyum8562@gmail.com>
2023-11-08 17:17:54 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,28 +1,19 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
- 
-		// nCk = n! / ((n-k)! * k!)
-		System.out.println(factorial(N) / (factorial(N - K) * factorial(K)));
-	}
- 
-	static int factorial(int N) {
-		// factorial(0) == 1 이다. 
-		if (N <= 1)	{
-			return 1;
-		}
-		return N * factorial(N - 1);
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int res = factorial(N) / (factorial(N-K) * factorial(K));
+        System.out.println(res);
+    }
+
+    private static int factorial(int N) {
+        if(N <= 1) return 1;
+        return N * factorial(N-1);
+    }
 }
