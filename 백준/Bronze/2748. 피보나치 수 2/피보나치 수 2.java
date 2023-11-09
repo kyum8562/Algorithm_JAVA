b2bceb718ead019549179f78ff2e3c22854d915a
giraffe_kim <kyum8562@gmail.com>
2023-11-09 15:56:07 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,19 +1,17 @@
 import java.io.*;
-
+import java.util.*;
 public class Main {
-    static long[] dp;
-    public static void main(String[] args) throws IOException {
+    static int N;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        dp = new long[N+1];
-
+        long[] dp = new long[90+1];
         dp[0] = 0;
         dp[1] = 1;
-
         for(int i = 2 ; i <= N ; i ++)
             dp[i] = dp[i-1] + dp[i-2];
-
+        
         System.out.println(dp[N]);
     }
 }
