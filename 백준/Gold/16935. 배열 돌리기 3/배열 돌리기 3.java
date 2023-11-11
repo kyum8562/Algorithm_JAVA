42e0b5222e6dd000e405ec63d5e48a5d852e65b2
giraffe_kim <kyum8562@gmail.com>
2023-11-11 15:13:41 +0900

    [Gold V] Title: 배열 돌리기 3, Time: 536 ms, Memory: 62956 KB -BaekjoonHub

--- 백준/Gold/16935. 배열 돌리기 3/배열 돌리기 3.java
+++ 백준/Gold/16935. 배열 돌리기 3/배열 돌리기 3.java
@@ -1,191 +1,158 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-// 새로운 배열을 만들어서 여기다가 하나씩 넣게
 public class Main {
-	
-	public static int[][] arr;
-	public static int[][] answer1;
-	
-	public static int N;
-	public static int M;
-	public static int R;
-	public static int new_N;
-	
-	// 델타 : {아래, 오른쪽, 위, 왼쪽}
-	public static int[] delta_x = {0, 1, 0, -1};
-	public static int[] delta_y = {-1, 0, 1, 0};
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		StringBuilder sb = new StringBuilder();
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		
-		new_N = Math.max(N, M);
-		
-		arr = new int[new_N][new_N]; // 입력받은 배열
-		
-		
-		for(int i = 0 ; i < N ; i ++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for(int j = 0 ; j < M ; j ++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int tcase = 1 ; tcase <= R ; tcase++) {
-			int type = Integer.parseInt(st.nextToken());
-			
-			if(type == 1)
-				type1();
-			else if(type == 2)
-				type2();
-			else if(type == 3)
-				type3();
-			else if(type == 4)
-				type4();
-			else if(type == 5)
-				type5();
-			else if(type == 6)
-				type6();
-		}
-		// 출력부
-		for(int i = 0 ; i < N ; i ++) {
-			for(int j = 0 ; j < M ; j ++) {
-					sb.append(arr[i][j] + " ");
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb);
-	}
-	
-	// type1: 상하 반전
-	public static void type1() {
-		answer1 = new int[new_N][new_N]; // 입력받은 배열
-		for(int i = 0 ; i < N ; i ++) {
-			for(int j = 0 ; j < M ; j ++) {
-				answer1[N-1-i][j] = arr[i][j];
-			}
-		}
-		arr = answer1;
-	}
-	
-	// type2: 좌우 반전
-	public static void type2() {
-		answer1 = new int[new_N][new_N]; // 입력받은 배열
-		
-		for(int i = 0 ; i < N ; i ++) {
-			for(int j = 0 ; j < M ; j ++) {
-				answer1[i][M-1-j] = arr[i][j];
-			}
-		}
-		
-		arr = answer1;
-	}
-	
-	// type3: 오른쪽 90도 회전
-	public static void type3() {
-		answer1 = new int[new_N][new_N]; // 입력받은 배열
+    static int N, M, R, newN;
+    static int[][] map, tmp;
+    static StringBuilder sb;
 
-		for(int i = 0 ; i < M ; i ++) {
-			for(int j = 0 ; j < N ; j ++) {
-				answer1[i][j] = arr[N-1-j][i];
-			}
-		}
-		N^=M;
-		M^=N;
-		N^=M;
-		
-		arr = answer1;
-	}
-	
-	// type4: 왼쪽 90도 회전
-	public static void type4() {
-		answer1 = new int[new_N][new_N]; // 입력받은 배열
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        newN = Math.max(N, M);
+        map = new int[newN][newN];
 
-		for(int i = 0 ; i < M ; i ++) {
-			for(int j = 0 ; j < N ; j ++) {
-				answer1[i][j] = arr[j][M-1-i];
-			}
-		}
-		
-		// swap
-		N^=M;
-		M^=N;
-		N^=M;
-		
-		arr = answer1;
-	}
-	
-	// type5: 4 분할 시계방향 이동
-	public static void type5() {
-		answer1 = new int[new_N][new_N]; // 입력받은 배열
-		
-		// 1 -> 2
-		for(int i = 0 ; i < N/2 ; i ++) {
-			for(int j = 0 ; j < M/2 ; j ++) {
-				answer1[i][j+M/2] = arr[i][j];
-			}
-		}
-		// 2 -> 3
-		for(int i = 0 ; i < N/2 ; i ++) {
-			for(int j = M/2 ; j < M ; j ++) {
-					answer1[i+N/2][j] = arr[i][j];
-			}
-		}
-		// 3 -> 4
-		for(int i = N/2 ; i < N ; i ++) {
-			for(int j = M/2 ; j < M ; j ++) {
-					answer1[i][j-M/2] = arr[i][j];
-			}
-		}
-		// 4 -> 1
-		for(int i = N/2 ; i < N ; i ++) {
-			for(int j = 0 ; j < M/2 ; j ++) {
-					answer1[i-N/2][j] = arr[i][j];
-			}
-		}
-		
-		arr = answer1;
-	}
-	
-	// type6: 4 분할 반시계방향 이동
-	public static void type6() {
-		answer1 = new int[new_N][new_N]; // 입력받은 배열
-		
-		// 1 -> 4
-		for(int i = 0 ; i < N/2 ; i ++) {
-			for(int j = 0 ; j < M/2 ; j ++) {
-				answer1[i+ N/2][j] = arr[i][j];
-			}
-		}
-		// 4 -> 3
-		for(int i = N/2 ; i < N ; i ++) {
-			for(int j = 0 ; j < M/2 ; j ++) {
-				answer1[i][j+M/2] = arr[i][j];
-			}
-		}
-		
-		// 3 -> 2
-		for(int i = N/2 ; i < N ; i ++) {
-			for(int j = M/2 ; j < M ; j ++) {
-				answer1[i-N/2][j] = arr[i][j];
-			}
-		}
-		// 2 -> 1
-		for(int i = 0 ; i < N/2 ; i ++) {
-			for(int j = M/2 ; j < M ; j ++) {
-				answer1[i][j-M/2] = arr[i][j];
-			}
-		}
-		
-		arr = answer1;
-	}
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int tcase = 1 ; tcase <= R ; tcase++) {
+            int type = Integer.parseInt(st.nextToken());
+            if(type == 1)
+                type1(); // 상하반전
+            else if(type == 2)
+                type2(); // 좌우반전
+            else if(type == 3)
+                type3(); // 오른쪽90도회전
+            else if(type == 4)
+                type4(); // 왼쪽90도회전
+            else if(type == 5)
+                type5(); // 4그룹 이동(1->2, 2->3, 3->4, 4->1)
+            else if(type == 6)
+                type6(); // 4그룹 이동(5와 반대방향)
+        }
+        printMap();
+    }
+
+    private static void type1() {
+        tmp = new int[newN][newN];
+
+        for(int i = 0 ; i < N ; i ++){
+            for(int j = 0 ; j < M ; j ++){
+                tmp[N-1-i][j] = map[i][j];
+            }
+        }
+
+        map = tmp;
+    }
+    private static void type2() {
+        tmp = new int[newN][newN];
+
+        for(int i = 0 ; i < N ; i ++){
+            for(int j = 0 ; j < M ; j ++){
+                tmp[i][M-1-j] = map[i][j];
+            }
+        }
+
+        map = tmp;
+    }
+    private static void type3() {
+        tmp = new int[newN][newN];
+
+        for(int i = 0 ; i < M ; i ++){
+            for(int j = 0 ; j < N ; j ++){
+                tmp[i][j] = map[N-1-j][i];
+            }
+        }
+        N^=M;
+        M^=N;
+        N^=M;
+        map = tmp;
+    }
+    private static void type4() {
+        tmp = new int[newN][newN];
+
+        for(int i = 0 ; i < M ; i ++){
+            for(int j = 0 ; j < N ; j ++){
+                tmp[i][j] = map[j][M-1-i];
+            }
+        }
+        N^=M;
+        M^=N;
+        N^=M;
+        map = tmp;
+    }
+    private static void type5() {
+        tmp = new int[newN][newN];
+        // 1
+        for(int i = 0 ; i < N/2 ; i ++){
+            for(int j = 0 ; j < M/2 ; j ++){
+                tmp[i][j+M/2] = map[i][j];
+            }
+        }
+        // 2
+        for(int i = 0 ; i < N/2 ; i ++){
+            for(int j = M/2 ; j < M ; j ++){
+                tmp[i+N/2][j] = map[i][j];
+            }
+        }
+        // 3
+        for(int i = N/2 ; i < N ; i ++){
+            for(int j = M/2 ; j < M ; j ++){
+                tmp[i][j-M/2] = map[i][j];
+            }
+        }
+        // 4
+        for(int i = N/2 ; i < N ; i ++){
+            for(int j = 0 ; j < M/2 ; j ++){
+                tmp[i-N/2][j] = map[i][j];
+            }
+        }
+        map = tmp;
+    }
+    private static void type6() {
+        tmp = new int[newN][newN];
+        // 1->4
+        for(int i = 0 ; i < N/2 ; i ++){
+            for(int j = 0 ; j < M/2 ; j ++){
+                tmp[i+N/2][j] = map[i][j];
+            }
+        }
+        // 2->1
+        for(int i = 0 ; i < N/2 ; i ++){
+            for(int j = M/2 ; j < M ; j ++){
+                tmp[i][j-M/2] = map[i][j];
+            }
+        }
+        // 3->2
+        for(int i = N/2 ; i < N ; i ++){
+            for(int j = M/2 ; j < M ; j ++){
+                tmp[i-N/2][j] = map[i][j];
+            }
+        }
+        // 4->3
+        for(int i = N/2 ; i < N ; i ++){
+            for(int j = 0 ; j < M/2 ; j ++){
+                tmp[i][j+M/2] = map[i][j];
+            }
+        }
+        map = tmp;
+    }
+
+    private static void printMap() {
+        sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(map[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 }
