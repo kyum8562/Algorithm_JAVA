a6533050c4eda3ee135665161c942fdb9aa2a44d
giraffe_kim <kyum8562@gmail.com>
2023-11-16 17:06:59 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,25 +1,25 @@
-let map = {
-    "zero": 0,
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-};
+let numbers = ["zero","one","two","three","four","five","six","seven","eight","nine"];
 function solution(s) {
-    s = s.replaceAll("zero", "0")
-    .replaceAll("one", "1")
-    .replaceAll("two", "2")
-    .replaceAll("three", "3")
-    .replaceAll("four", "4")
-    .replaceAll("five", "5")
-    .replaceAll("six", "6")
-    .replaceAll("seven", "7")
-    .replaceAll("eight", "8")
-    .replaceAll("nine", "9")
-    return parseInt(s);
+    let ans = s;
+    for(let i = 0 ; i < numbers.length ; i ++){
+        let arr = ans.split(numbers[i]);
+        ans = arr.join(i);
+        // console.log(ans)
+        
+    }
+    return parseInt(ans);
 }
+
+// function solution(s) {
+//     s = s.replaceAll("zero", "0")
+//     .replaceAll("one", "1")
+//     .replaceAll("two", "2")
+//     .replaceAll("three", "3")
+//     .replaceAll("four", "4")
+//     .replaceAll("five", "5")
+//     .replaceAll("six", "6")
+//     .replaceAll("seven", "7")
+//     .replaceAll("eight", "8")
+//     .replaceAll("nine", "9")
+//     return parseInt(s);
+// }
