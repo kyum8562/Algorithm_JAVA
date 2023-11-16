bb30c10a0e7774760a6c414a8b7680d4907d7897
giraffe_kim <kyum8562@gmail.com>
2023-11-16 16:55:33 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,25 @@
+let map = {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+};
+function solution(s) {
+    s = s.replaceAll("zero", "0")
+    .replaceAll("one", "1")
+    .replaceAll("two", "2")
+    .replaceAll("three", "3")
+    .replaceAll("four", "4")
+    .replaceAll("five", "5")
+    .replaceAll("six", "6")
+    .replaceAll("seven", "7")
+    .replaceAll("eight", "8")
+    .replaceAll("nine", "9")
+    return parseInt(s);
+}
