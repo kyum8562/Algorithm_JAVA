dddf188bb5a77691378b4b68033d0d65efae0043
giraffe_kim <kyum8562@gmail.com>
2023-11-16 17:51:34 +0900

    [Gold III] Title: 소수의 연속합, Time: 196 ms, Memory: 26856 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -18,25 +18,23 @@ public class Main {
         for (int i = 1; i <=N ; i++)
             if(!isPrime[i]) list.add(i);
  
-        int s = 0;
-        int e = 0;
-        int sum = 0;
-        int count = 0;
+        int s, e, sum, cnt;
+        s = e = sum = cnt = 0;
         while (true){
             if(sum >= N) sum -= list.get(s++);
             else if(e == list.size()) break;
             else sum += list.get(e++);
             
-            if(N==sum) count++;
+            if(N==sum) cnt++;
         }
  
-        System.out.println(count);
+        System.out.println(cnt);
     }
     
 	private static void eratos() {
         isPrime[0] = isPrime[1] = true;
-        
-		for(int i = 2 ; i*i <= N ; i ++) {
+        int sqrtNum = (int)(Math.sqrt(N));
+		for(int i = 2 ; i <= sqrtNum ; i ++) {
 			if(!isPrime[i]) {
 				for(int j=i*i; j<=N; j+=i)
 					isPrime[j]=true;      
