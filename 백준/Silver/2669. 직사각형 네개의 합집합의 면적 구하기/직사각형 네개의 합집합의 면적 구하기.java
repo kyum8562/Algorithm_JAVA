d77ee7d30df238081965f8646f01f147e12056c0
giraffe_kim <kyum8562@gmail.com>
2023-11-25 15:02:41 +0900

    [Silver V] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int ans = 0;
+    static int[][] map;
+    public static void main(String[] args) throws  IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        map = new int[101][101];
+
+        int stage = 0;
+        while(stage++ < 4){
+            st = new StringTokenizer(br.readLine());
+            int r1 = Integer.parseInt(st.nextToken());
+            int c1 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
+
+            marking(r1, c1, r2, c2);
+            ans += (r2-r1) * (c2-c1);
+        }
+        calculating();
+        System.out.println(ans);
+    }
+
+    private static void calculating() {
+        for(int i = 1 ; i <= 100 ; i ++){
+            for(int j = 1 ; j <= 100 ; j ++){
+                if(map[i][j] >= 2){
+                    int tmp = map[i][j] - 1;
+                    map[i][j] -= tmp;
+                    ans -= tmp;
+                }
+            }
+        }
+    }
+    // 입력받으면서 map에 카운팅하고,  맨해튼 거리 r2-r1 * c2-c1의 곱을 저장, 이후에 map에 카운팅 개수 -1 만큼 빼줌
+
+    private static void marking(int r1, int c1, int r2, int c2) {
+        for(int r = r1 ; r < r2 ; r ++){
+            for(int c = c1 ; c < c2 ; c ++){
+                map[r][c] ++;
+            }
+        }
+    }
+}
